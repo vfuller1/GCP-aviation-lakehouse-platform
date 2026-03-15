5b3d32a1000d6dd96daf7b4add6077719cce70bc
vfuller1 <victor.g.fuller@gmail.com>
2026-03-15 14:08:36 -0500

    fix: use CI-compatible GCP credentials path in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket  = "lakehouse-gcp-state"
     prefix  = "terraform/state"
-    credentials = "c:/Users/vfull/gcp-keys/kubernetes-365701-7477240054cf.json"
+    credentials = "${path.module}/gcp-key.json"
   }
 }
