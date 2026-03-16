d4ec7ab0eb2c5f90cfb7d45423d227aa65c935ae
vfuller1 <victor.g.fuller@gmail.com>
2026-03-15 19:50:13 -0500

    fix: use static credentials path for Terraform backend

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket  = "lakehouse-gcp-state"
     prefix  = "terraform/state"
-    credentials = "${path.module}/gcp-key.json"
+    credentials = "gcp-key.json"
   }
 }
